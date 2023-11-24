fee396e41053c19d015ffcc97ea5efb86ec9f936
Brunobrno <brunovontor@gmail.com>
2023-11-24 21:02:42 +0100

    Json zprovozněn
    JSON obsah se tiskne na obrazovku ale zdroj souboru se vyvolává skrz HTML % static %

--- info_app/static/javascript/script.js
+++ info_app/static/javascript/script.js
@@ -1,10 +1,12 @@
 function json_cake_load(){
-    const jsonFile = "{% static 'jsonFiles/cake.json' %}";
-
+  const jsonFile = document.currentScript.getAttribute('input-json');
+  
   fetch(jsonFile)
-    .then(response => response.json())
+    .then(response => response.json())//.then je operace která čeká na slib (zrovna tady je response.json)
     .then(data => {
       console.log(data);
+      console.log(data.secret);
+      document.getElementById("json-print").innerHTML = String(data.secret);
     })
 }
 
