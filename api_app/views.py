9916307e40add36b68a3ca402dc7c2eb8b2542fa
Brunobrno <brunovontor@gmail.com>
2023-11-25 21:51:08 +0100

    nastavení routování a zmazání jsonOutput.py
    - jsonOutput.py: zkoušel jsem experimentovat ale už na to nezbyl čas

--- api_app/views.py
+++ api_app/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
-def load_index(request):
+def load_api(request):
     template = loader.get_template('api_index.html')
     return HttpResponse(template.render())
--- info_app/urls.py
+++ info_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from api_app.views import load_api
 
 urlpatterns = [
-    path('', views.load_index, name='index'),
-    path('api/', views.load_index, name='api'),
+    path('', views.load_info, name='index'),
+    path('api/', load_api, name='api'),
 ]
