5c9289cbed8a448711d8b0896f4e74a404e8839c
Brunobrno <brunovontor@gmail.com>
2023-11-29 20:45:39 +0100

    zprovoznění API
    - odstraněn starý html
    - redirect v metodě byl vyměněn za JsonResponse

--- api_app/views.py
+++ api_app/views.py
@@ -1,8 +1,16 @@
-# Create your views here.
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
+import os
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.staticfiles import finders
+
 
 def load_api(request):
-    template = loader.get_template('api_index.html')
-    return HttpResponse(template.render())
+    # Read JSON data from a file
+    json_file_path = finders.find('jsonFiles/cake.json')
+    
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    # Use JsonResponse to send JSON response
+    return JsonResponse(data)
