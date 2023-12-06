b39bf9c0e5a52f6721d90f00055cf80e483e16bb
Brunobrno <brunovontor@gmail.com>
2023-12-06 22:35:11 +0100

    update /lecturer
    - přidal jsem javascript pro zobrazení JSONU
    - dál jsem nějaké základní Bootstrap styly

--- globalstatic/javascript/lecturer_json.js
+++ globalstatic/javascript/lecturer_json.js
@@ -4,7 +4,23 @@ function json_lecturer_load(){
     fetch(jsonFile)
       .then(response => response.json())//.then je operace která čeká na slib (zrovna tady je response.json)
       .then(data => {
-        document.getElementById("test").innerHTML = String(data.location);
+        document.getElementById("name").innerHTML = String(data.title_before + " " + data.first_name + " " + data.middle_name);
+        document.getElementById("pozice").innerHTML = String(data.title_after);
+        document.getElementById("lecturer_picture").src = String(data.picture_url);
+        document.getElementById("location").innerHTML = String(data.location);
+        document.getElementById("claim").innerHTML = String(data.claim);
+        document.getElementById("bio").innerHTML = String(data.bio);
+
+        document.getElementById("tag_name_0").innerHTML = String(data.tags[0].name);
+        document.getElementById("tag_name_3").innerHTML = String(data.tags[3].name);
+        document.getElementById("tag_name_7").innerHTML = String(data.tags[7].name);
+        /* MYSLEL JSEM ŽE SE MÁJÍ VYPSAT VŠECHNY TAGY ALE NAKONEC JENOM SPECIFICKÉ (NECHAL JSEM TUHLE ČÁST KÓDU PROTOŽE BY SE MOHL POUŽÍT BUDOUCNU)
+        for (let i = 0; i < jsonData.tags.length; i++) {
+          document.getElementById(String(data.tags[i].name)).innerHTML = String(data.tags[i].name);
+        }*/
+
+
+
       })
   }
   
